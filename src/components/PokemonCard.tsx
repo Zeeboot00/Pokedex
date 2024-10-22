ff73b144316fac8b0fda06d35eccc4b79eb1589e
ALEXANDRE JANASIEWICZ <alexandrejanasiewicz@gmail.com>
2024-10-22 12:15:59 +0200

    bulbiiiii

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,12 @@
+function PokemonCard() {
+  return (
+    <figure>
+      <img
+        src="https://i.etsystatic.com/14697815/r/il/caa02d/4198715682/il_570xN.4198715682_nm7f.jpg"
+        alt="bulbasaur"
+      />
+      <figcaption>Bulbasaur</figcaption>
+    </figure>
+  );
+}
+export default PokemonCard;
