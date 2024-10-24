35b79ba2c5358f4d10d8ebdeaec427ad95d74c5f
ALEXANDRE JANASIEWICZ <alexandrejanasiewicz@gmail.com>
2024-10-24 11:36:21 +0200

    plus de pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 import PokemonCard from "./components/PokemonCard";
@@ -9,16 +10,49 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "mew",
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
   },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+    imgSrc:
+      "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/151.png",
+  }
 ];
 
 function App() {
-	return (
-		<div>
-			<PokemonCard pokemon={pokemonList[0]} />
-		</div>
-	);
+  const [count, setCount] = useState(0);
+
+  const incremente = () => {
+    if (count < pokemonList.length - 1) {
+      setCount(count + 1);
+    }
+  };
+
+  const decremente = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <PokemonCard pokemon={pokemonList[count]} />
+      <button onClick={decremente}>Précédent</button>
+      <button onClick={incremente}>Suivant</button>
+    </div>
+  );
 }
 
 export default App;
